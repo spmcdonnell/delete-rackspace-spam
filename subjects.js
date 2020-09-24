423e9e0bd9219a4de411764cc7b172bca4ce8614
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2020-09-23 20:49:38 -0700

    Move lists of senders/subject lines to delete in separate files

---
+++ subjects.js
@@ -0,0 +1,23 @@
+// Subject lines to mark for deletion (these can be a substring of whole subject line - should be made lowercase)
+var keywords = [
+    'submission',
+    'contact',
+    'newsletter',
+    'lead',
+    'comment',
+    'disk usage warning',
+    'reservations',
+    '[spam]',
+    'optinmonster',
+    'automatic',
+    'something i think you',
+    'sage pay',
+    'covid-19',
+    'turbomeeting',
+    'shipworks',
+    'asana',
+    'hh imaging - new file uploaded',
+    'scheduled your payment'
+];
+
+export default keywords;
